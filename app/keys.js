0d416bfb076df11e14c8889e79dffd5097034c82
Thijs Raymakers <thijs@raymakers.nl>
2017-12-29 20:15:54 +0100

    Add random key generator

---
+++ app/keys.js
@@ -0,0 +1,17 @@
+var crypto = require('crypto');
+
+function randomValueBase64(len) {
+    return crypto.randomBytes(Math.ceil(len * 3 / 4))
+        .toString('base64')   // convert to base64 format
+        .slice(0, len)        // return required number of characters
+        .replace(/\+/g, '0')  // replace '+' with '0'
+        .replace(/\//g, '0'); // replace '/' with '0'
+}
+
+function getRandomKey() {
+    return randomValueBase64(128);
+}
+
+module.exports = {
+    getRandomKey: getRandomKey
+}
