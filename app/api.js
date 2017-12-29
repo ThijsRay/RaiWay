2fb13b4f0a034f0bc6f2c107f2693b6b2d4db2e9
Thijs Raymakers <thijs@raymakers.nl>
2017-12-29 20:16:32 +0100

    Move files to /app directory

--- app/api.js
+++ app/api.js
@@ -5,6 +5,9 @@ var router = express.Router();
 // Load the RPC handler. The parameter is the URL of the node.
 const rpc = require('./rpc.js').rpc_actions("http://[::1]:7076");
 
+// Load keys
+const keys = require('./keys.js');
+
 router.get('/', function(req, res) {
     res.send('Please specify the API function you want to use.');
 });
@@ -13,6 +16,10 @@ router.get('/get_block_count', function(req, res) {
     rpc.get_block_count(function(body) {
         res.send(body);
     });
+});
+
+router.post('/request-payment-token', function(req, res) {
+    res.send(keys.getRandomKey());
 });
 
 module.exports = router;
